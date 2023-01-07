89c31a76ad116dc530d71d9bd6cbf5f90ad4303d
takaya <takaya_ishiyama0810@yahoo.co.jp>
2023-01-07 17:48:18 +0900

    バックエンド諸々

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -11,57 +11,18 @@ class UserListView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny,) 
-    # permission_classes = [IsAdminUser]
 
 class UserDetailView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    # permission_classes = [IsAdminUser]
-    permission_classes = (AllowAny,) 
+    permission_classes = [IsAdminUser]
+    # permission_classes = (AllowAny,) 
 
 class DeleteUserView(generics.RetrieveDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    # permission_classes = [IsAdminUser]
-    permission_classes = (AllowAny,) 
+    permission_classes = [IsAdminUser]
+    # permission_classes = (AllowAny,) 
 
-@csrf_exempt
-def user_list(request):
-    if request.method == 'GET':
-        items = User.objects.all()
-        serializer = UserSerializer(items, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = UserSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-
-# @csrf_exempt
-# def user_detail(request, pk):
-#     try:
-#         item = User.objects.get(pk=pk)
-#     except User.DoesNotExist:
-#         return HttpResponse(status=404)
-
-#     if request.method == 'GET':
-#         serializer = UserSerializer(item)
-#         return JsonResponse(serializer.data)
-
-#     elif request.method == 'PUT':
-#         data = JSONParser().parse(request)
-#         serializer = UserSerializer(item, data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data)
-#         return JsonResponse(serializer.errors, status=400)
-
-#     elif request.method == 'DELETE':
-#         item.delete()
-#         return HttpResponse(status=204)
 
 
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -76,8 +76,8 @@ REST_FRAMEWORK = {
 }
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT'),
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,
