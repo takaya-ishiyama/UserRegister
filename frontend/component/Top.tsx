b8aebe5b372795a83ec6b29f658eeeb5eb71cf2d
takaya <takaya_ishiyama0810@yahoo.co.jp>
2023-01-27 01:00:14 +0900

    fetch user function

--- frontend/component/Top.tsx
+++ frontend/component/Top.tsx
@@ -1,17 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { Button } from '@chakra-ui/react';
-import { GetUser } from '../feature/fetchUser';
+import {Login} from '../feature/fetchUser';
 
 export const Top = () => {
-    // const {data, isLoading, isError, error} = useQuery(['users'], fetchCurrentUser);
-    // useEffect(()=>{console.log(data)},[data]);;
-    const [user, setUser] = useState();
+    const User = Login("takaya","taka1480");
 
-
-    // function handleClick() {
-    //     console.log(user)
-    // }
-    useEffect(()=>{setUser(GetUser("takaya", "taka1480"))},[])
     return (
         <>
             TOP
--- frontend/feature/fetchUser.ts
+++ frontend/feature/fetchUser.ts
@@ -50,74 +50,22 @@ async function fetchUser(): Promise<Response> {
 }
 
 
-
-export const GetUser = (username:string, password: string) => {
-  const [user, setUser] = useState<any>();
-  const [is_accesstoken, setIsAccessToken] = useState(false);
-  const [is_refreshtoken, setIsRefreshToken] = useState(false);
-  let cookies = parseCookies()
-
-  // useEffect(()=>{auth(username, password)},[]);
-  useEffect(()=>{login(username, password)},[]);
-  useEffect(()=>{cookies = parseCookies()},[parseCookies()]);
-
-  const login = async (username: string, password: string)=> {
-    const resp = await fetchToken(username, password);
-    if (resp.ok) {
-      const tokenData = await resp.json();
-      setCookie(null, 'accessToken', tokenData.access, {
-          maxAge: 60 * 60,/*60min X 60second*/
-      })
-      setCookie(null, 'refreshToken', tokenData.refresh, {
-          maxAge: 30 * 24 * 60 * 60,/* 24h X 60min X 60second*/
-      })
-      setIsRefreshToken(true);
-      const respuser = await fetchUser();
-      const _user = await respuser.json()
-      return _user
-    } else {
-      console.log("error : fetch access token");
-      return;
-    }
-  };
-  
-  const getToken = async () => {
-    const resp = await fetchNewToken();
-    if(resp.ok){
-      const tokenData = await resp.json();
-      setCookie(null, 'accessToken', tokenData.access,{
-        maxAge: 60 * 60,/*60min X 60second*/ 
-      })
-      setIsAccessToken(true)
-    }else{
-      console.log("error : fetch refresh token ")
-      return;
-    }
+export const Login = async (username: string, password: string) => {
+  const resp = await fetchToken(username, password);
+  if (resp.ok) {
+    const tokenData = await resp.json();
+    setCookie(null, 'accessToken', tokenData.access, {
+        maxAge: 60 * 60,/*60min X 60second*/
+    })
+    setCookie(null, 'refreshToken', tokenData.refresh, {
+        maxAge: 30 * 24 * 60 * 60,/* 24h X 60min X 60second*/
+    })
+    // setIsRefreshToken(true);
+    const respuser = await fetchUser();
+    const User = await respuser.json();
+    return User;
+  } else {
+    console.log("error : fetch access token");
+    return;
   }
-  const logout = (): void => {
-    const url = makeUrl("/logout/");
-    fetch(url, {
-      method: "POST",
-      credentials: "include"
-    });
-  };
-
-   async function auth (username: string, password: string) {
-    if(cookies){
-      try{
-        const resp = await login(username, password);
-        setUser(resp)
-      }catch{
-        await getToken();
-        const resp = await fetchUser();
-        const _user = await resp.json();
-        setUser(_user);
-      }
-    }else{
-      console.log("error auth")
-      return;
-    }
-  };
-  return user;
 };
-
